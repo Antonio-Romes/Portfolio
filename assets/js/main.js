7d319b22625d620e3b83dc3916b69364e00531df
Antonio-Romes <antonioromes1@hotmail.com>
2024-01-25 23:13:16 -0300

    subi projeto com home e sobre-mim atualizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
  
+ window.addEventListener('resize', adjustContentHeight);
+ window.addEventListener('load', adjustContentHeight);
+
+ function adjustContentHeight() {
+     var navHeight = document.querySelector('nav').offsetHeight;
+     document.querySelector('main').style.marginTop = navHeight + 'px';
+ }
