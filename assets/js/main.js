27a7ce789509b89f2c55c562adcbe59cc9ab0fd4
Antonio-Romes <antonioromes1@hotmail.com>
2024-01-27 23:55:31 -0300

    construir o layout com bootstrap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,32 @@
  
- window.addEventListener('resize', adjustContentHeight);
- window.addEventListener('load', adjustContentHeight);
+  
+ let link = document.querySelectorAll('nav a') ;
+ 
 
- function adjustContentHeight() {
-     var navHeight = document.querySelector('nav').offsetHeight;
-     document.querySelector('main').style.marginTop = navHeight + 'px';
+ link.forEach(item => {
+    item.addEventListener("click", minhaFuncao)
+ })
+
+ function minhaFuncao (e){
+    let valueNavHeight = document.querySelector('nav').offsetHeight;
+
+    let valueId = e.target.attributes.value.value;
+
+    let element = document.getElementById(valueId);
+
+    element.style.paddingTop = (valueNavHeight) +"px"; 
+
+    
  }
+ 
+ window.addEventListener("scroll", function() {
+     var scrollTop = window.scrollY || document.documentElement.scrollTop; 
+
+    let valueNavHeight = document.querySelector('nav').offsetHeight;
+
+    if(scrollTop <= valueNavHeight){
+        let element = document.getElementById('home');
+
+         element.style.paddingTop = (scrollTop) +"px";
+    } 
+ });
