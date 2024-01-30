4cf373fbdf9f8b03556d62dc575d28bdfc5ad482
Antonio-Romes <antonioromes1@hotmail.com>
2024-01-30 00:48:22 -0300

    adicina animação com a biblioteca aos.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
  
   
+ AOS.init();
+
  let link = document.querySelectorAll('nav a') ;
  
 
@@ -30,3 +32,53 @@
          element.style.paddingTop = (scrollTop) +"px";
     } 
  });
+
+
+const by = (seletor) => document.querySelector(seletor);
+const $typingText = by(".type-text");
+const $cursor = by(".cursor");
+
+const words = ["Desenvolvedor Web."];
+
+const delay = {
+    typing: 200,
+    keeping: 1000,
+    erasing: 100,
+    word: 2000,
+}
+
+const sleep = (ms) => {
+
+    return new Promise ((resolve) =>{
+        setTimeout(() => resolve(),ms);
+    });
+
+};
+
+const type = async(word)=>{
+    $cursor.classList.add("typing");
+    for(const char of word){
+        $typingText.textContent += char;
+        await sleep(delay.typing);
+    }
+
+    $cursor.classList.remove("typing");
+    await sleep(delay.keeping);
+
+    for(let i = 1; i <= word.length; i++){
+        $typingText.textContent = word.substring(0, word.length -i);
+        await sleep(delay.erasing);
+    }
+}
+
+const loop = async (wordIndex = 0) =>{
+    await type(words[wordIndex % words.length])
+    setTimeout(async () => {
+        await loop(wordIndex + 1)
+    },delay.word);
+}
+
+document.addEventListener("DOMContentLoaded",() =>{
+    loop();
+});
+
